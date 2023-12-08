1c8b30bbb1ae1e44dc422705460386807638755c
wangzhouquan <wangzhouquan78@gmail.cn>
2023-12-08 18:23:16 +0800

    init

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,40 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use std::sync::atomic::{AtomicU64, Ordering};
+
+use thiserror::Error;
+
+#[derive(Error, Debug)]
+pub enum GeneratorError {}
+
+pub trait Generator {
+    fn generator_id(&self) -> Result<u64, GeneratorError> {
+        return Ok(1);
+    }
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+struct Section(u64, u64);
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+impl Section {
+    fn new(start: u64, end: u64) -> Self {
+        Section(start, end)
+    }
+}
+
+pub trait Allocator<T: Into<GeneratorError>> {
+    fn allocator(step_size: u32) -> Result<Section, T>;
+}
+
+struct generator {
+    now_id: AtomicU64,
+    max_id: u64,
+}
+
+impl Generator for generator {
+    fn generator_id(&self) -> Result<u64, GeneratorError> {
+        let v = self.now_id.fetch_add(1, Ordering::Relaxed) + 1;
+        if v < self.max {
+            return Ok(v);
+        }
+
+        Ok(1)
     }
 }
